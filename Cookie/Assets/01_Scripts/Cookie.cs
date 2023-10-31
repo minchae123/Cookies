26005644da897927022281b21da3a6980320cd93
minchae123 <08minchae17@gmail.com>
2023-10-31 14:41:35 +0900

    coo

---
+++ Cookie/Assets/01_Scripts/Cookie.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cookie : MonoBehaviour
+{
+	public int cookieID = 0;
+	public int cookiePrice = 0;
+}
